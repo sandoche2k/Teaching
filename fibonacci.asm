67ce5518aab506caca7d5f67110c7581ae54674a
Sandoche BALAKRICHENAN <sandoche2k@gmail.com>
2025-08-05 11:53:14 +0200

    Update fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,59 +1,49 @@
-.data
-prompt:     .asciiz "Fibonacci Sequence:\n"
-newline:    .asciiz "\n"
+# Compute first twelve Fibonacci numbers and put in array, then print
+      .globl main
+      .data 
+fibs: .word   0 : 12        # "array" of 12 words to contain fib values
+size: .word  12             # size of "array 
+#	.byte -0x100  
+      .text
+main:  
+      la   $t0, fibs        # load address of array
+      la   $t5, size        # load address of size variable
+      lw   $t5, 0($t5)      # load array size
+      li   $t2, 1           # 1 is first and second Fib. number
+      sw   $t2, 0($t0)      # F[0] = 1
+      sw   $t2, 4($t0)      # F[1] = F[0] = 1
+      addi $t1, $t5, -2     # Counter for loop, will execute (size-2) times
+loop: lw   $t3, 0($t0)      # Get value from array F[n] 
+      lw   $t4, 4($t0)      # Get value from array F[n+1]
+      add  $t2, $t3, $t4    # $t2 = F[n] + F[n+1]
+      sw   $t2, 8($t0)      # Store F[n+2] = F[n] + F[n+1] in array
+      addi $t0, $t0, 4      # increment address of Fib. number source
+      addi $t1, $t1, -1     # decrement loop counter
+      bgtz $t1, loop        # repeat if not finished yet.
+      la   $a0, fibs        # first argument for print (array)
+      add  $a1, $zero, $t5  # second argument for print (size)
+      jal  print            # call print routine. 
+      li   $v0, 10          # system call for exit
+      syscall               # we are out of here.
 
-.text
-.globl main
+#########  routine to print the numbers on one line. 
 
-main:
-    li $t0, 0          # F(0)
-    li $t1, 1          # F(1)
-    li $t4, 10         # Number of terms (you can change this)
-    
-    li $v0, 4
-    la $a0, prompt     # Print the prompt
-    syscall
-
-    # Print F(0)
-    move $a0, $t0
-    li $v0, 1
-    syscall
-
-    li $v0, 4          # Print newline
-    la $a0, newline
-    syscall
-
-    # Print F(1)
-    move $a0, $t1
-    li $v0, 1
-    syscall
-
-    li $v0, 4
-    la $a0, newline
-    syscall
-
-    li $t2, 2          # Counter starts from 2
-
-loop:
-    bge $t2, $t4, exit # Exit when counter reaches n
-
-    add $t3, $t0, $t1  # F(n) = F(n-1) + F(n-2)
-
-    move $t0, $t1      # Update F(n-2)
-    move $t1, $t3      # Update F(n-1)
-
-    # Print F(n)
-    move $a0, $t3
-    li $v0, 1
-    syscall
-
-    li $v0, 4
-    la $a0, newline
-    syscall
-
-    addi $t2, $t2, 1   # Increment counter
-    j loop
-
-exit:
-    li $v0, 10         # Exit program
-    syscall
+      .data
+space:.asciiz  " "          # space to insert between numbers
+head: .asciiz  "The Fibonacci numbers are:\n"
+      .text
+print:add  $t0, $zero, $a0  # starting address of array
+      add  $t1, $zero, $a1  # initialize loop counter to array size
+      la   $a0, head        # load address of print heading
+      li   $v0, 4           # specify Print String service
+      syscall               # print heading
+next: lw   $a0, 0($t0)      # load fibonacci number for syscall
+      li   $v0, 1           # specify Print Integer service
+      syscall               # print fibonacci number
+      la   $a0, space       # load address of spacer for syscall
+      li   $v0, 4           # specify Print String service
+      syscall               # output string
+      addi $t0, $t0, 4      # increment address
+      addi $t1, $t1, -1     # decrement loop counter
+      bgtz $t1, next        # repeat if not finished
+      jr   $ra              # return
